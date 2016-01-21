5ee148e84719fc5e14baadee30c3341c0d3cb109
Sateesh Kumar <sateekot@cisco.com>
2016-01-21 23:05:36 +0530

    Variables and Strings

---
+++ VariablesAndStrings/VariablesAndStrings.js
@@ -0,0 +1,54 @@
+var myName = "SateeshKumar";
+var wishMsg = "Hello "+myName+", Welcome to JavaScript Learning.."
+console.log(wishMsg);
+
+
+
+/*
+*   Variables
+*/
+
+var suit;
+var card;
+suit = "hearts";
+console.log(suit);
+
+var greeting = "Hello World!";
+
+console.log(greeting);
+
+//tweat
+
+var tweep = "@sateesh";
+console.log(tweep);
+
+var message = "Hey! I need some help with programming";
+
+var tweet = tweep + ": "+message;
+
+console.log(tweet);
+
+
+var age = 8;
+var facebookMsg = "You're only "+ age +", too young to facebook";
+
+console.log(facebookMsg);
+
+var burritoPrice = 5.99;
+
+var taxRate = 0.09;
+
+var totalTax = burritoPrice*taxRate;
+
+console.log("Tax is :"+totalTax);
+
+var totalBurritoPrice = burritoPrice+totalTax;
+
+console.log("The Total Burrito Price is "+totalBurritoPrice);
+
+
+
+var totalNoOfBurritors = 5;
+var totalCostOfBurritos = (totalNoOfBurritors*burritoPrice)*taxRate;
+
+console.log("The Cost of "+totalNoOfBurritors +" is "+totalCostOfBurritos);
